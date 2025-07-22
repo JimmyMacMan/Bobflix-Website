eec98b5a0217fc366e498762bf5dd9269d8bd21e
JimmyMacMan <121jimmey121@gmail.com>
2025-07-22 18:22:13 +0100

    rest

--- scipts.js
+++ scipts.js
@@ -1,9 +1,18 @@
-fetch('https://ac.bobflix.uk/', { mode: 'no-cors' })
-            .then(() => {
-                document.getElementById('ac-status').textContent = 'Online';
-                document.getElementById('ac-status').style.color = '#00e676';
-            })
-            .catch(() => {
-                document.getElementById('ac-status').textContent = 'Offline';
-                document.getElementById('ac-status').style.color = '#ff4444';
-            });
+const url = "https://ac.bobflix.uk";
+fetch("https://ac.bobflix.uk")
+    .then(response => response.text())
+    .then(text => {
+        if (text.includes("Bad gateway")) {
+            document.getElementById('ac-status').textContent = 'Offline';
+            document.getElementById('ac-status').style.color = '#ff4444';
+        } else {
+            document.getElementById('ac-status').textContent = 'Online';
+            document.getElementById('ac-status').style.color = '#00e676';
+        }
+    })
+    .catch(() => {
+        document.getElementById('ac-status').textContent = 'Offline';
+        document.getElementById('ac-status').style.color = '#ff4444';
+    });
+
+    
